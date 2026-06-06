d35c54901d948bf1e6424c9c92341d8409138a96
SofiaDenisse19 <sofiaquispearela@gmail.com>
2026-06-06 17:33:18 -0500

    Cargar imágenes

---
+++ src/main/java/com/example/sotelogourmet/SotelogourmetApplication.java
@@ -0,0 +1,13 @@
+package com.example.sotelogourmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotelogourmetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SotelogourmetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sotelogourmet/SotelogourmetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sotelogourmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SotelogourmetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
